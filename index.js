30a9db9f808c4c736bc8e2ad67a34cbf3510dc6d
KARTHIK <104157027+46Karthik@users.noreply.github.com>
2024-01-01 18:49:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
+const puppeteer = require('puppeteer');
 const app = express();
 const port = 3000;
 
@@ -10,33 +10,83 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('Server is running!');
 });
-const headers = {
-  'Accept': 'application/json, text/plain, */*',
-  'Content-Type': 'application/xml',
-  'User-Agent': 'axios/1.6.3',
-  'Accept-Encoding': 'gzip, compress, deflate, br'
-};
-// Handle POST requests on the "/post-data" route
-app.post('/modification', (req, res) => {
-  // Access the JSON data sent in the request body
-  const postData = req.body;
-  const url = postData.URL;
-  let modified_URL = ''; // Declare the variable here
 
-  axios.head(url, { maxRedirects: 5, validateStatus: null,  headers: headers })
-    .then(response => {
-      modified_URL = response.request._redirectable._currentUrl;
-      // Log the received data
-      console.log("modified_URL:",modified_URL);
-console.log("response",response)
-      // Send a JSON response
-      res.json({ message: 'POST data received successfully', modified_URL: modified_URL });
-    })
-    .catch(error => {
-      console.error('Error:', error.message);
-      // Send a JSON response with an empty modified_URL
-      res.json({ message: 'POST data received successfully', modified_URL: modified_URL });
+// Handle POST requests on the "/post-data" route
+app.post('/modification', async (req, res) => {
+  try {
+    // Access the JSON data sent in the request body
+    const postData = req.body;
+    const keywordinput = postData.keyword;
+    const keyword = keywordinput;
+
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+
+    // Wrap the entire scraping process in a Promise
+    const details = await new Promise(async (resolve, reject) => {
+      try {
+        console.log("started");
+
+        await page.goto(`https://masstamilan.day/?s=${keyword}`, { waitUntil: 'networkidle2', timeout: 0 });
+
+        const Search_result = await page.$x(`(//*[@class="entry-header"]//@href)[1]`);
+        const Search_result_link = await page.evaluate(el => el.textContent, Search_result[0]);
+        console.log(Search_result_link);
+
+        await page.goto(`${Search_result_link}`, { waitUntil: 'networkidle2', timeout: 0 });
+        await page.waitForTimeout(2000);
+
+        const Movie_information = await page.$x(`//table//tbody`);
+        const tittle_image = await page.$x(`(//header//img)[3]//@src`);
+
+        const result = [{
+          keyword: keyword,
+          Movie_information: await page.evaluate(el => el.innerText, Movie_information[0]),
+          tittle_image: await page.evaluate(el => el.textContent, tittle_image[0]),
+        }];
+
+        const table_row02 = await page.$x('//table[2]//tr');
+        
+        if (!table_row02) {
+          const table_row = await page.$x('//table//tr');
+          console.log(table_row.length, "length");
+          
+          for (let j = 1; j < table_row.length; j++) {
+            const song_name = await page.$x(`((//div[@class="display-posts-listing image-left"])[1]//a[@class="title"])[${j}]`);
+            const song = await page.$x(`((//div[@class="display-posts-listing image-left"])[1]//a[@class="excerpt-more"]//@href)[${j}]`);
+
+            result.push({
+              song_name: await page.evaluate(el => el.innerText, song_name[0]),
+              song: await page.evaluate(el => el.textContent, song[0]),
+            });
+          }
+        } else {
+          console.log(table_row02.length, "length");
+          for (let j = 1; j < table_row02.length; j++) {
+            const song_name = await page.$x(`(//table[2]//tr[${j}]//td)[1]`);
+            const song = await page.$x(`(//table[2]//tr[${j}]//td[3])//@href`);
+
+            result.push({
+              song_name: await page.evaluate(el => el.innerText, song_name[0]),
+              song: await page.evaluate(el => el.textContent, song[0]),
+            });
+          }
+        }
+
+        resolve(result);
+      } catch (error) {
+        console.log(error);
+        reject(error);
+      }
     });
+
+    res.json({ message: 'POST data received successfully', Data: details });
+    console.log(details, "details");
+    await browser.close();
+  } catch (error) {
+    console.error(error);
+    res.json({ error: 'Error processing request' });
+  }
 });
 
 app.listen(port, () => {
